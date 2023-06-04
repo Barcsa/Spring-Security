d0616b2c969c89f2b2941352bc9cde65735fbf20
Barcsa <barcsathemiddleman@gmail.com>
2023-06-04 14:19:12 +0300

    commit

---
+++ src/test/java/com/barcsa/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barcsa.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
